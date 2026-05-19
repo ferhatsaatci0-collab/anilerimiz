56091308ba5239c099ae6bbe12d9b6f75017ddd2
ferhatsaatci0-collab <ferhatsaatci0@gmail.com>
2026-05-19 14:57:04 +0300

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "1b0ac5c8-1c07-4dc8-b1d7-47556dcf8bf1",
+    });
+  });
+</script>
